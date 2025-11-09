6fd92a6ee737867cd8f140b6baf1fbc6bb0ee45b
Kowal <jan.kowalski4b@o2.pl>
2025-11-09 19:37:23 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,25 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import accountRoutes from './routes/accountRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import transactionRoutes from './routes/transactionRoutes.js';
+import budgetRoutes from './routes/budgetRoutes.js';
+import savingsRoutes from './routes/savingsRoutes.js';
+import reportRoutes from './routes/reportRoutes.js';
 
 dotenv.config({quiet: true});
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
+router.use('/accounts', accountRoutes);
+router.use('/categories', categoryRoutes);
+router.use('/transactions', transactionRoutes);
+router.use('/budgets', budgetRoutes);
+router.use('/savings', savingsRoutes);
+router.use('/reports', reportRoutes);
 
 const app = express();
 app.use(express.json());
